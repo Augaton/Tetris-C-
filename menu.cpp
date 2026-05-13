989ada52ff760d098a124bc1ef7573542fd1b464
Augaton <Mulleraurelien@hotmail.fr>
2026-05-13 09:24:54 +0200

    [Fix] Resolution ajusted automaticly

--- menu.cpp
+++ menu.cpp
@@ -33,12 +33,15 @@ void main() {
 })";
 
 
-menu::menu(sf::RenderWindow& window, sf::Font &font1){
-    AddrWindow = &window;
-    font = &font1;
-    if (!blurShader.loadFromMemory(blur_frag, sf::Shader::Fragment)) throw std::runtime_error("Failed to load shader");
-    blurShader.setUniform("resolution", sf::Vector2f(900, 540));
-    blurShader.setUniform("radius", 5.f);   
+void menu::Flou(sf::Texture& texture){
+    sf::Sprite sprite(texture);
+    sf::Vector2f currentRes(static_cast<float>(AddrWindow->getSize().x), 
+                            static_cast<float>(AddrWindow->getSize().y));
+    
+    blurShader.setUniform("resolution", currentRes);
+    
+    AddrWindow->clear();
+    AddrWindow->draw(sprite, &blurShader);
 }
 
 menu::~menu(){}
