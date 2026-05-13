8a8d66b36cbad53613d99da8a8d9ba475f4fdf4a
Augaton <Mulleraurelien@hotmail.fr>
2026-05-13 09:26:23 +0200

    [Fix] correct blur resolution and construction

--- menu.cpp
+++ menu.cpp
@@ -33,15 +33,13 @@ void main() {
 })";
 
 
-void menu::Flou(sf::Texture& texture){
-    sf::Sprite sprite(texture);
-    sf::Vector2f currentRes(static_cast<float>(AddrWindow->getSize().x), 
-                            static_cast<float>(AddrWindow->getSize().y));
+menu::menu(sf::RenderWindow& window, sf::Font &font1){
+    AddrWindow = &window;
+    font = &font1;
+    if (!blurShader.loadFromMemory(blur_frag, sf::Shader::Fragment)) 
+        throw std::runtime_error("Failed to load shader");
     
-    blurShader.setUniform("resolution", currentRes);
-    
-    AddrWindow->clear();
-    AddrWindow->draw(sprite, &blurShader);
+    blurShader.setUniform("radius", 5.f);   
 }
 
 menu::~menu(){}
@@ -55,6 +53,11 @@ void centrerTexte(sf::Text& texte) {
 
 void menu::Flou(sf::Texture& texture){
     sf::Sprite sprite(texture);
+    sf::Vector2f currentRes(static_cast<float>(AddrWindow->getSize().x), 
+                            static_cast<float>(AddrWindow->getSize().y));
+    
+    blurShader.setUniform("resolution", currentRes);
+    
     AddrWindow->clear();
     AddrWindow->draw(sprite, &blurShader);
 }
